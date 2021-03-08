0123eb3a4598fc65c9ac65eb331dadd5c9b46676
KISHORE SHANKAR <a.kishore.shankar.2000@gmail.com>
2021-03-08 15:09:35 +0530

    Added Components

---
+++ components/Caritem/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Caritem = () => {
+    return (
+        <div>
+            <View>
+                <View>
+                    <View></View>
+                </View>
+            </View>
+        </div>
+    )
+}
+
+export default Caritem
